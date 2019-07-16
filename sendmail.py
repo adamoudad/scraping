e8c31d123a933f0c5067ad09d118a75ea16a292b
adamoudad <adam.oudad@gmail.com>
2019-07-17 06:22:09 +0900

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,25 +1,30 @@
+# If using gmail, enable 2-step verification beforehand
+# https://support.google.com/accounts/answer/185839
+
 import smtplib
 from getpass import getpass
 
-def send_email(body, subject):
+def send_email(body, subject, sender, receiver):
   server = smtplib.SMTP('smtp.gmail.com', 587)
   server.ehlo()
   server.starttls()
   server.ehlo()
 
-  password = getpass("Your mail password: ")
-  server.login('myemail@gmail.com', password)
+  password = getpass("Sender mail password:")
+  server.login(sender, password)
 
-  msg = f"Subject: {subject}\n\n{body}"
+  message = f"Subject: {subject}\n\n{body}"
   server.sendmail(
-      'myemail',
-      'mysecondemail',
-      msg
+      sender,
+      receiver,
+      message
   )
+
+if __name__ == "__main__":
+  sender = input("Sender:")
+  receiver = input("Receiver:")
+  subject = input('Subject:') or "Hello !"
+  body = input("Body:") or "Sample text."
   
+  send_email(body, subject, sender, receiver)
   print("Email has been sent !")
-
-subject = 'Interesting product from craigslist'
-body = 'Check craigslist link : ' + URL
-
-send_email(body, subject)
